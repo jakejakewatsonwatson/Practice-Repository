d20fed65ded7ec8e75351dae24c58215c7bb55ab
Jake Watson <jakejakewatsonwatson@gmail.com>
2018-07-18 10:01:55 -0500

    Added the ability for the user to choose the price per cup and how many cups they plan to sell.

--- Lemonade Stand/Program.cs
+++ Lemonade Stand/Program.cs
@@ -9,7 +9,12 @@ namespace ConsoleApp15
             Console.WriteLine("What would you like to name your lemonade stand?");
             string nameOfStand = Console.ReadLine();
 
+            Console.WriteLine("How many cups do you plan on selling?");
+            var Inventory = Console.ReadLine();
 
+            Console.WriteLine("How much does each cup of lemonade cost?");
+            var PricePerCup = Console.ReadLine();
+            
 
 
 
