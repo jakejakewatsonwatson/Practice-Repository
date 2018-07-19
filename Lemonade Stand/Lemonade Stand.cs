efa0e14e8e9467f5ff8f5bc9a8e0bf3426351c2f
Jake Watson <jakejakewatsonwatson@gmail.com>
2018-07-19 12:56:59 -0500

    Made the Stand Class more functional, added in methods for all math in the main function. Gave the user to create as many lemonade stands as they want.

--- Lemonade Stand/Lemonade Stand.cs
+++ Lemonade Stand/Lemonade Stand.cs
@@ -7,7 +7,38 @@ namespace ConsoleApp15
     public class Stand
     {
         public string nameOfStand { get; set; }
+        public string strInventory { get; set; } 
+        public string strPricePerCup { get; set; }
+        public string strCostOfProduction { get; set; }
+        
+        public decimal Inventory()
+        {
+            return Int32.Parse(strInventory);
+        }
 
+        public decimal PricePerCup()
+        {
+            return Int32.Parse(strPricePerCup);
+        }
 
+        public decimal CostOfProduction()
+        {
+            return Int32.Parse(strCostOfProduction);
+        }
+
+        public decimal TotalSpentOnSupplies()
+        {
+            return Inventory() * CostOfProduction();
+        }
+
+        public decimal TotalRevenue()
+        {
+            return Inventory() * PricePerCup();
+        }
+
+        public decimal TotalProfit()
+        {
+            return TotalRevenue() - TotalSpentOnSupplies();
+        }
     }
 }
--- Lemonade Stand/Program.cs
+++ Lemonade Stand/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ConsoleApp15
 {
@@ -6,29 +7,39 @@ namespace ConsoleApp15
     {
         static void Main(string[] args)
         {
-            string stand1 = new LemonadeStands();
+            List<Stand> stands = new List<Stand>();
 
-            Console.WriteLine("What would you like to name your lemonade stand?");
-            string nameOfStand = Console.ReadLine();
+            Console.WriteLine("How many lemonade stands would you like to open this summer?");
+            string strNumberOfStands = Console.ReadLine();
+            decimal numberOfStands = Int32.Parse(strNumberOfStands);
 
-            Console.WriteLine("How many cups do you plan on selling?");
-            string strInventory = Console.ReadLine();
+            for (int i = 0; i < numberOfStands; i++)
+            {
+                Stand stand = new Stand();
+                
+                Console.WriteLine("What would you like to name this lemonade stand?");
+                stand.nameOfStand = Console.ReadLine();
+                stands.Add(stand);
 
-            Console.WriteLine("How much does each cup of lemonade cost?");
-            string strPricePerCup = Console.ReadLine();
+                Console.WriteLine("How many cups do you plan on selling at this location?");
+                stand.strInventory = Console.ReadLine();
 
-            Console.WriteLine("How much does each cup cost for you to make? (cost of sugar, water, lemons, etc)?");
-            string strCostOfProduction = Console.ReadLine();
+                Console.WriteLine("How much does each cup of lemonade cost at this location?");
+                stand.strPricePerCup = Console.ReadLine();
 
-            decimal Inventory = Int32.Parse(strInventory);
-            decimal PricePerCup = Int32.Parse(strPricePerCup);
-            decimal CostOfProduction = Int32.Parse(strCostOfProduction);
-            decimal totalSpentOnSupplies = Inventory * CostOfProduction;
-            decimal totalRevenue = Inventory * PricePerCup;
-            decimal totalProfit = totalRevenue - totalSpentOnSupplies;
-            
-            Console.WriteLine($"{nameOfStand} brought in ${totalProfit} in profit, while spending ${totalSpentOnSupplies} on supplies and generated ${totalRevenue} in total revenue");
-            
+                Console.WriteLine("How much does each cup cost for you to make? (cost of sugar, water, lemons, etc)?");
+                stand.strCostOfProduction = Console.ReadLine();
+                
+                //Console.WriteLine($"{stand.nameOfStand} brought in ${stand.TotalProfit()} in profit, while spending ${stand.TotalSpentOnSupplies()} on supplies and generated ${stand.TotalRevenue()} in total revenue");
+                
+            }
+
+            Console.ReadLine();
+            foreach (Stand stand in stands)
+            {
+                Console.WriteLine($"{stand.nameOfStand} brought in ${stand.TotalProfit()} in profit, while spending ${stand.TotalSpentOnSupplies()} on supplies and generated ${stand.TotalRevenue()} in total revenue");
+            }
+
             Console.ReadLine();
         }
             
